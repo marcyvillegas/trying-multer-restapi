75512199c4dbdcafceda330a26b900a33907d223
Marcy Villegas <58241136+marcyvillegas@users.noreply.github.com>
2021-10-25 21:22:34 +0800

    successfully uploaded the imaeg

--- fetch-api.js
+++ fetch-api.js
@@ -1,12 +1,34 @@
 
 const display = async () => {
 
-    const response = await fetch("http://localhost:8000/products");
+    const response = await fetch("http://localhost:8000/products/all");
     const data = await response.json();
 
-    data.forEach( (data) => {
-        document.querySelector("#prodName").textContent = data.Product_Name;
-        document.querySelector("#prodType").textContent = data.Product_Type;
+    let output = "";
+    let container = document.querySelector(".container");
+
+    data.forEach((data) => {
+        // document.querySelector("#prodName").textContent = data.Product_Name;
+        // document.querySelector("#prodType").textContent = data.Product_Type;
+        // document.querySelector("#prodImg").textContent = data.Product_Img;
+
+        let content = `
+        
+        <div class="">
+        <p>Product Name: <span id="prodName">${data.Product_Name}</span></p>
+    </div>
+
+    <div>
+        <p>Product Type: <span id="prodType">${data.Product_Type}</span></p>
+    </div>
+
+    <div>
+        <img src="${data.Product_Img}" alt="">
+    </div>
+        
+        `;
+
+        container.innerHTML = content;
     });
 }
 
